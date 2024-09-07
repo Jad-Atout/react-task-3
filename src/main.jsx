6d63c69e99b3e441169719319b6e036963dfbb2f
Jad-Atout <JadAtout.2003@outlook.sa>
2024-09-08 01:42:17 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
+import {StrictMode} from "react";
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <StrictMode>
     <App />
+    </StrictMode>,
 )
